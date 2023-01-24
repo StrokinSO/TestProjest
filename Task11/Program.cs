0dc2d979421fa46dbe15dfca431b1e261d4d59a2
Sergey <f0reks@mail.ru>
2023-01-24 16:59:56 +0700

    Изменения от 24.01.23

---
+++ Task11/Program.cs
@@ -0,0 +1,19 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Трехзначное число: {number}");
+
+int res = RemoveSecondDigit(number);
+Console.WriteLine($"Результат: {res}");
+
+int RemoveSecondDigit(int num)
+{
+    int num1 = number / 100;
+    int num3 = number % 10;
+    int result = num1 * 10 + num3;
+    return result;
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+    Console.WriteLine("Не выходной день");
+else if (number == 2)
+    Console.WriteLine("Не выходной день");
+else if (number == 3)
+    Console.WriteLine("Не выходной день");
+else if (number == 4)
+    Console.WriteLine("Не выходной день");
+else if (number == 5)
+    Console.WriteLine("Не выходной день");
+else if (number == 6)
+    Console.WriteLine("Выходной");
+else if (number == 7)
+    Console.WriteLine("Выходной");
+else
+    Console.WriteLine("Введено некорректное значение");
